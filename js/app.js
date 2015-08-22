d0c1f5dd907b4117048f55caa054c288604afd82
Chris Garvin <chris.garvin@me.com>
2015-08-21 17:01:15 -0700

    changed to one page design. need to work on scaling issues still

--- js/app.js
+++ js/app.js
@@ -1,10 +1,10 @@
 $(document).ready(function(){
 
   $.getJSON("data/data.json", function(data){
-    $('.products').prepend("<h2>" + data.response.products[0].name.toUpperCase() + "</h2>");
+    // $('.products').prepend("<h2>" + data.response.products[0].name.toUpperCase() + "</h2>");
     for(var i = 0; i < data.response.products[0].options.length; i++){
       var item = data.response.products[0].options[i];
-      $('.products').append("<a href='#quantity'><img data-id='" + item.color + "' class='product' src='" + item.image + "'></a>");
+      $('.products').append("<span class='productContainer'><img class='product' data-id='" + item.color + "' src='" + item.image + "'></span>");
       $('.product').click(function(){
         $('.cart')[0].innerHTML = "<h3>" + $(this).attr("data-id").toUpperCase() + "</h3><img src='" + this.src + "'>";
       });
