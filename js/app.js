58b7a40731ae4ea5ca84a2d5198bc05e428d8b41
Chris Garvin <chris.garvin@me.com>
2015-08-21 18:26:23 -0700

    added filler text for info and show/hide on click of nav links at footer

--- js/app.js
+++ js/app.js
@@ -1,5 +1,20 @@
 $(document).ready(function(){
 
+//inputting random text into info temporarily
+
+  $.getJSON('http://hipsterjesus.com/api/', {paras:1}).done(function(data) {
+            $('.info').html( data.text );
+        });
+
+  $('.color').click(function(){
+    $('.info').hide();
+    $('.products').show();
+  })
+  $('.information').click(function(){
+    $('.products').hide();
+    $('.info').show();
+  })
+
   $.getJSON("data/data.json", function(data){
     // $('.products').prepend("<h2>" + data.response.products[0].name.toUpperCase() + "</h2>");
     for(var i = 0; i < data.response.products[0].options.length; i++){
